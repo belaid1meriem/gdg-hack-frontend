530539cf35ebea13dd1d20606d594ee1603c4e9b
belaid1meriem <mm_belaid@esi.dz>
2025-02-08 05:02:30 +0100

    service exchange integration (some ui and add form)

--- gdghack/src/components/StudentServiceExchanging.jsx
+++ gdghack/src/components/StudentServiceExchanging.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { useParams } from 'react-router-dom';
 import { indicatorsContext } from '../contexts/indicatorContext';
 import { FaCirclePlus } from "react-icons/fa6";
+import AddServiceExchange from './AddServiceExchange';
 
 
 function StudentServiceExchanging() {
@@ -13,8 +14,30 @@ function StudentServiceExchanging() {
   const closeForm = () => setIsFormOpen(false)
 
   return (
-    <div>
+    <div className='flex flex-col gap-4 px-8 py-4'>
+      <FaCirclePlus className='text-3xl self-end cursor-pointer' onClick={openForm}/>
+      {services.map((service) => (
+      <div 
+        className="flex flex-col items-start gap-2 border rounded-md shadow p-4 relative hover:scale-105 transition-all duration-500" 
+        key={service.id}
+      >
+        <span className="absolute top-2 right-2 text-md font-medium text-gray-500">
+          {service.status}
+        </span>
+        <h4 className="font-semibold">
+          {service.student1_id+"  "+service.student1_name} ↔ {service.student2_id+"  "+service.student2_name}
+        </h4>
+        <p className="text-sm text-gray-700">🎯 {service.task1}</p>
+        <p className="text-sm text-gray-700">🎯 {service.task2}</p>
+        <span className="text-xs text-gray-500">
+          {new Date(service.created_at).toLocaleDateString()}
+        </span>
+      </div>
+    ))}
 
+      {isFormOpen && (
+        <AddServiceExchange closeForm={closeForm}/>
+      )}
     </div>
   )
 }
@@ -26,29 +49,28 @@ const useServiceExchange = (id) => {
   const [ services, setServices] = useState([])
   const { setLoading, errorMsg, setErrorMsg } = useContext(indicatorsContext)
   useEffect(() => {
-    const fetchVirtualExp = async () => {
+    const fetchServiceExchange = async () => {
       setLoading(true);
       try {
-        const response = await axios.get(`http://127.0.0.1:8000/student/task_exchanges/${id}/`, {
+        const response = await axios.get(`http://127.0.0.1:8000/student/task_exchangesList/${id}/`, {
           headers: {
             'Authorization': `Bearer ${localStorage.getItem('token')}`
           }
         });
-        setExp(response.data);
-        console.log(response.data)
+        setServices(response.data);
+        console.log(response.data);
       } catch (error) {
         if(error.status !== 404){
-          console.error('Error fetching student info:', error);
+         console.error('Error fetching student info:', error);
          setErrorMsg('Error fetching student info. Please try again later.');
         }
-       
       }
       finally {
         setLoading(false);
       }
     };
 
-    if (id) fetchVirtualExp(); // Ensure id exists before making a request
+    if (id) fetchServiceExchange(); // Ensure id exists before making a request
   }, [id]);
 
   return [ services, setServices];
